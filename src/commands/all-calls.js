89fb5884528b92d442c86f4fb50aabdc6acd7c59
Ross Basarevych <basarevych@gmail.com>
2017-09-28 03:19:07 +0300

    Dynamic ACL

--- src/commands/all-calls.js
+++ src/commands/all-calls.js
@@ -138,7 +138,7 @@ class AllCallsCommand {
             if (!ctx.user.authorized)
                 return false;
 
-            if (!ctx.user.isAllowed(0)) {
+            if (!ctx.user.isAllowed(this._app.get('acl').get('cdr'))) {
                 await ctx.reply('В доступе отказано');
                 await scene.sendMenu(ctx);
                 return true;
--- src/commands/missed-calls.js
+++ src/commands/missed-calls.js
@@ -66,7 +66,7 @@ class MissedCallsCommand {
             if (!ctx.user.authorized)
                 return false;
 
-            if (!ctx.user.isAllowed(0)) {
+            if (!ctx.user.isAllowed(this._app.get('acl').get('cdr'))) {
                 await ctx.reply('В доступе отказано');
                 await scene.sendMenu(ctx);
                 return true;
